aa99a7e0dbc6570b36447e64ce89d208a33198d0
Md. Ridoy Hasan Kamrul <80632331+Md-Ridoy-Hasan-Kamrul@users.noreply.github.com>
2025-06-23 20:12:53 +0600

    remove header

--- src/utils/translations.jsx
+++ src/utils/translations.jsx
@@ -1,17 +1,5 @@
 export const translations = {
   en: {
-    // Header
-    header: {
-      home: "Home (Search)",
-      agents: "Agents",
-      about: "About",
-      contact: "Contact",
-      login: "Login",
-      register: "Register",
-      addFreeAd: "Add Free Ad",
-      kuwaitRealEstate: "Kuwait Real Estate",
-    },
-
     footer: {
       titleFast: "Properties for rent",
       footerChildone: "Apartments for rent",
@@ -167,18 +155,6 @@ export const translations = {
       },
     },
     ar: {
-      // Header
-      header: {
-        home: "الرئيسية (بحث)",
-        agents: "المكاتب",
-        about: "من نحن",
-        contact: "اتصل بنا",
-        login: "تسجيل الدخول",
-        register: "تسجيل جديد",
-        addFreeAd: "اضافة اعلان مجان",
-        kuwaitRealEstate: "العقار الكويتي",
-      },
-
       // Home Page
       home: {
         bannerTitle: "عقارات للايجار | عقارات للبيع",
