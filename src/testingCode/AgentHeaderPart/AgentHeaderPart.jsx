e9e88b8ff08b528d3347fa369567d75fa11e62d4
shakilmonsi <107542773+shakilmonsi@users.noreply.github.com>
2025-06-29 15:06:11 +0600

    agent  page complte

--- src/testingCode/AgentHeaderPart/AgentHeaderPart.jsx
+++ src/testingCode/AgentHeaderPart/AgentHeaderPart.jsx
@@ -4,13 +4,12 @@ import { MdKeyboardArrowRight } from "react-icons/md";
 const AgentHeaderPart = () => {
   return (
     <div className="border-1 border-t border-gray-300 bg-[#F5F7F9]">
-      <div className="mx-auto w-full max-w-7xl bg-[hsl(240,25%,97%)] px-5 text-center">
+      <div className="mx-auto w-full max-w-7xl bg-[#F5F7F9] px-5 text-center">
         <div className="flex items-center px-1 py-2 text-[12px] font-normal text-[#556885]">
           {" "}
-          <Link
-            to="/"
-            className="px-1 text-[12px] font-normal text-[#556885]"
-          >agent</Link>
+          <Link to="/" className="px-1 text-[12px] font-normal text-[#556885]">
+            agent
+          </Link>
           <>
             <MdKeyboardArrowRight />
           </>
